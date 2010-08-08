79864974a41025b4aff7ac9b75b3accd1456701d
John Reese <jreese@leetcode.net>
2010-08-08 18:15:11 -0400

    Added more to combine namespace

--- combine/__init__.py
+++ combine/__init__.py
@@ -8,9 +8,12 @@ class CombineError(Exception):
     def __str__(self):
         return repr(self.value)
 
+from combine.hash import sha1
 from combine.formats import Archive, File
 from combine.config import Config
 from combine.package import Package
 from combine.manifest import Manifest
+from combine.uri import URI
+
 from combine.diff import Diff
 
