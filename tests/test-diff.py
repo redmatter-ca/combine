96ab788fe8d0a9c915e3770f5586fbd02285a350
John Reese <jreese@leetcode.net>
2010-08-19 20:43:30 -0400

    Set up python logging module to output messages for test scripts

--- tests/test-diff.py
+++ tests/test-diff.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2010 John Reese
 # Licensed under the MIT license
 
+import logging
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
 from combine import Diff, Config
 
 Config.read("test.conf")
--- tests/test-update.py
+++ tests/test-update.py
@@ -1,6 +1,9 @@
 # Copyright (c) 2010 John Reese
 # Licensed under the MIT license
 
+import logging
+logging.basicConfig(level=logging.INFO, format="%(message)s")
+
 from combine import Update
 
 u = Update("1", "file:///tmp/combine/Linux/manifest/1.yaml")
